fe76b66abdfcebb3c8075bb349a217948eed3dfb
cezzarmonteiro <cezr.monteiro@gmail.com>
2023-03-02 19:02:25 -0300

    criado componente de icone

--- pages/src/components/header/header.jsx
+++ pages/src/components/header/header.jsx
@@ -2,14 +2,17 @@ import styles from './Header.module.css'
 
 import Title from "../title/title"
 import Subtitle from "../subtitle/subtitle"
-
+import Icon from "../icon/Icon"
 
 function Header () {
 return (
   <div className={styles.header}>
  <Title>Jogo da Velha</Title>
  <Subtitle>Criado por Cézar Monteiro</Subtitle>
-  </div>
+<div className={styles.iconContent}>
+<Icon iconName="github" link="https://github.com/cezzarmonteiro" />
+</div>
+</div>
 )
 
 }
