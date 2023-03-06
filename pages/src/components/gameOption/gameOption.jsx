e6b3e7f53966dcd4e2391958dbe30ff4894b9769
cezzarmonteiro <cezr.monteiro@gmail.com>
2023-03-06 11:53:21 -0300

    criada a logica do jogo

--- pages/src/components/gameOption/gameOption.jsx
+++ pages/src/components/gameOption/gameOption.jsx
@@ -2,14 +2,14 @@ import styles from './gameOption.module.css'
 import Icon from '../icon/Icon'
 
 
-function GameOption ({ status }) {
+function GameOption ({ status,onClick }) {
   return (
-    <div className={styles.gameOption}>
+    <div className={styles.gameOption} onClick={onClick}>
       {
-        status === 1 && <Icon iconName="x" size="25px" />
+        status === -1 && <Icon iconName="x" size="25px" />
       }
        {
-        status === -1 && <Icon iconName="x" size="25px" />
+        status === 1 && <Icon iconName="circle" size="25px" />
       }
     </div>
   )
